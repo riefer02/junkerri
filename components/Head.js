70fd6cbfe683dada91f432f17bcbf90e3fcafb6a
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-02-06 21:46:20 -0600

    implement initial rich google schema ld+json

--- components/Head.js
+++ components/Head.js
@@ -9,11 +9,9 @@ const NextHead = ({ title, description, keywords, image, author }) => {
     "Junkerri Art | Playful Abstract Creations by Nepali Artist Aastha Karki";
 
   let url;
-  if (typeof window !== "undefined") {
-    url = window.location.href;
-  } else {
-    url = "https://junkerri.com/";
-  }
+  typeof window !== "undefined"
+    ? (url = window.location.href)
+    : (url = "https://junkerri.com/");
 
   const baseURL =
     process.env.NODE_ENV === "development"
@@ -21,7 +19,41 @@ const NextHead = ({ title, description, keywords, image, author }) => {
       : "https://junkerri.com/";
 
   const ogURL = `${baseURL}junkerri-open-graph.jpg`;
-  console.log({ env: process.env.NODE_ENV, baseURL, ogURL });
+
+  const schemaMarkup = {
+    "@context": "https://schema.org/",
+    "@type": "LocalBusiness",
+    name: "Junkerri Art",
+    image: [ogURL],
+    description:
+      "Discover the vibrant and captivating world of Junkerri Art, created by Nepali artist Aastha Karki. Explore our unique collection of playful abstract works.",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "7728 Manassas Dr.",
+      addressLocality: "Austin",
+      addressRegion: "Texas",
+      postalCode: "78745",
+      addressCountry: "United States",
+    },
+    email: "junkerriart@gmail.com",
+    geo: {
+      "@type": "GeoCoordinates",
+      latitude: 30.441738,
+      longitude: -97.766586,
+    },
+    url: baseURL,
+    sameAs: ["https://www.instagram.com/junkerri"],
+    offers: {
+      "@type": "Offer",
+      priceCurrency: "USD",
+      price: "35.00",
+      availability: "https://schema.org/InStock",
+      seller: {
+        "@type": "Organization",
+        name: "Junkerri Arts Local Store",
+      },
+    },
+  };
 
   return (
     <Head>
@@ -38,6 +70,11 @@ const NextHead = ({ title, description, keywords, image, author }) => {
         content={description || defaultDescription}
       />
       <meta name="twitter:image" content={image || ogURL} />
+      {schemaMarkup && (
+        <script type="application/ld+json">
+          {JSON.stringify(schemaMarkup)}
+        </script>
+      )}
     </Head>
   );
 };
