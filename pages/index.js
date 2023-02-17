64b189755a2e74e6b775c27faa04afcfc35287f6
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-02-17 16:55:33 -0600

    update order

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <NextHead />
       <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
-          {products.reverse().map((product) => (
+          {products.map((product) => (
             <ProductCard
               key={product.id}
               disabled={disabled}
