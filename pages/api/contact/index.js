df9995ea2bddaea2d5e81390aff4661981678bcc
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-02-26 14:27:14 -0600

    remove extra config from fetch

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  const { name, email, message } = req.body;
+  const { name, email, message } = JSON.parse(req.body);
 
   try {
     const transporter = nodemailer.createTransport({
@@ -14,18 +14,25 @@ export default async function handler(req, res) {
       },
     });
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_FROM,
-      to: process.env.EMAIL_TO,
-      subject: `Junkerri Art Contact Form | ${name}`,
-      text: message,
-      html: `<p>${message}</p><p>From: ${name} &lt;${email}&gt;</p>`,
-    });
+    await transporter.sendMail(
+      {
+        from: process.env.EMAIL_FROM,
+        to: process.env.EMAIL_TO,
+        subject: `Junkerri Art Contact Form | ${name}`,
+        text: message,
+        html: `<p>${message}</p><p>From: ${name} &lt;${email}&gt;</p>`,
+      },
+      function (err) {
+        if (err) console.log(err);
+      }
+    );
 
     res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Error sending email" });
+    res
+      .status(500)
+      .json({ message: "Error sending email", error: error.message });
   }
 }
 
--- pages/contact.js
+++ pages/contact.js
@@ -11,15 +11,15 @@ const ContactForm = () => {
     try {
       await fetch("/api/contact", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
+        // headers: {
+        //   "Content-Type": "application/json",
+        //   // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        // redirect: "follow", // manual, *follow, error
+        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify({ name, email, message }), // body data type must match "Content-Type" header
       });
       alert("Your message has been sent!");
