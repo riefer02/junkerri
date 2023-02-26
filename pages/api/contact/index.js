4979a41c06d2e55d1bfe99e065d96977f6b489af
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-02-26 14:36:19 -0600

    add name to createTransport config

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
 
   try {
     const transporter = nodemailer.createTransport({
+      name: process.env.SMPT_HOST,
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
       secure: true,
