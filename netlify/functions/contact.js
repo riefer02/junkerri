e46723c14dde4ce261200c861a62e098805734a7
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-03-02 18:12:01 -0600

    install sparkpost

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,40 +1,38 @@
-const nodemailer = require("nodemailer");
+const SparkPost = require("sparkpost");
 
 exports.handler = async function (event, context) {
   const { name, email, message } = JSON.parse(event.body);
 
   try {
-    const transporter = nodemailer.createTransport({
-      name: process.env.DOMAIN_NAME,
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
+    const client = new SparkPost(process.env.SPARKPOST_API_KEY, {
+      debug: true,
     });
 
-    await transporter.sendMail(
-      {
-        from: process.env.EMAIL_FROM,
-        to: process.env.EMAIL_TO,
+    const payload = {
+      options: {
+        sandbox: false,
+      },
+      content: {
+        from: "contact@junkerri.com",
         subject: `Junkerri Art Contact Form | ${name}`,
-        text: message,
         html: `<p>${message}</p><p>From: ${name} &lt;${email}&gt;</p>`,
       },
-      function (err, info) {
-        if (err) console.log(err);
-        if (info) console.log(info);
-      }
-    );
+      recipients: [{ address: "andrew.riefenstahl@gmail.com" }],
+    };
+
+    const response = await client.transmissions.send(payload);
+    const { something, id } = response;
+    const responseObj = { something, id };
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Email sent successfully" }),
+      body: JSON.stringify({
+        message: "Email sent successfully",
+        response: responseObj,
+      }),
     };
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({
--- pages/contact.js
+++ pages/contact.js
@@ -11,17 +11,8 @@ const ContactForm = () => {
 
     try {
       await fetch("/.netlify/functions/contact", {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        // mode: "cors", // no-cors, *cors, same-origin
-        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: "same-origin", // include, *same-origin, omit
-        // headers: {
-        //   "Content-Type": "application/json",
-        //   // 'Content-Type': 'application/x-www-form-urlencoded',
-        // },
-        // redirect: "follow", // manual, *follow, error
-        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({ name, email, message }), // body data type must match "Content-Type" header
+        method: "POST",
+        body: JSON.stringify({ name, email, message }),
       });
       alert("Your message has been sent!");
       setName("");
