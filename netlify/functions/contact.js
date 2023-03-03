827f3d96be816f08082409531bd00d90e142a9c6
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-03-02 18:30:35 -0600

    add env var back

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -4,23 +4,22 @@ exports.handler = async function (event, context) {
   const { name, email, message } = JSON.parse(event.body);
 
   try {
-    const client = new SparkPost(process.env.SPARKPOST_API_KEY, {
-      debug: true,
-    });
+    const client = new SparkPost(process.env.SPARKPOST_API_KEY);
 
     const payload = {
       options: {
         sandbox: false,
       },
       content: {
-        from: "contact@junkerri.com",
+        from: process.env.EMAIL_FROM,
         subject: `Junkerri Art Contact Form | ${name}`,
         html: `<p>${message}</p><p>From: ${name} &lt;${email}&gt;</p>`,
       },
-      recipients: [{ address: "andrew.riefenstahl@gmail.com" }],
+      recipients: [{ address: process.env.EMAIL_TO }],
     };
 
     const response = await client.transmissions.send(payload);
+    // Fixes a circular json error
     const { something, id } = response;
     const responseObj = { something, id };
 
